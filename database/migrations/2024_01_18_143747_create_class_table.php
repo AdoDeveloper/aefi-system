e0134d50c96d24e5d5ab6e4ad30c978f1538b941
Adolfo Ernesto Cortez Barrera <ayudacuenta541@gmail.com>
2024-01-18 14:53:38 -0600

    migrations class

--- database/migrations/2024_01_18_143747_create_class_table.php
+++ database/migrations/2024_01_18_143747_create_class_table.php
@@ -11,17 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('class', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->tinyInteger('user_type')->default(3);
+            $table->tinyInteger('status')->default(0);
             $table->tinyInteger('is_delete')->default(0);
-            $table->string('user_photo')->nullable()->default('default.jpg');
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -31,7 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('class');
     }
 };
-

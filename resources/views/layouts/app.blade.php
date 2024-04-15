87ef4680b3accd649ea71e661050e5586fdc06bb
Adolfo Ernesto Cortez Barrera <105835711+AdoDeveloper@users.noreply.github.com>
2024-04-15 15:28:26 -0600

    se cambió posicion del index.php y cambio de enlaces locales por cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{!empty($header_title) ? $header_title : ''}} - AEFI</title>
-  <link rel="icon" href="{{url('public/images/logo-uso.ico')}}" type="image/x-icon">
+  <title>{{ !empty($header_title) ? $header_title : '' }} - AEFI</title>
+  <link rel="icon" href="https://res.cloudinary.com/dv6gogl5y/image/upload/v1705699875/images/x5nawtzhtsbb3jfrgc3s.jpg" type="image/x-icon">
 
   <style>
     /* Estilo personalizado para la barra de desplazamiento */
@@ -30,31 +30,42 @@
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{url('public/plugins/fontawesome-free/css/all.min.css')}}">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{url('public/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{url('public/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="{{url('public/plugins/jqvmap/jqvmap.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{url('public/dist/css/adminlte.min.css')}}">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{url('public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{url('public/plugins/daterangepicker/daterangepicker.css')}}">
-  <!-- summernote -->
-  <link rel="stylesheet" href="{{url('public/plugins/summernote/summernote-bs4.min.css')}}">
+ <!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+<!-- Tempusdominus Bootstrap 4 -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/4.6.0/css/tempusdominus-bootstrap-4.min.css">
+
+<!-- iCheck -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css">
+
+<!-- JQVMap -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqvmap/1.5.1/jqvmap.min.css">
+
+<!-- Theme style -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/css/adminlte.min.css">
+
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/1.13.0/css/OverlayScrollbars.min.css">
+
+<!-- Daterange picker -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.1.0/daterangepicker.css">
+
+<!-- Summernote -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.css">
+
+  
+  
+
+  <!-- Tailwind CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed pt-0 mt-0">
 <div class="wrapper">
 
   <!-- Preloader 
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{url('public/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ url('public/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
   </div>-->
 
   <!-- Welcome message -->
@@ -79,40 +90,53 @@
 
 </div>
 <!-- ./wrapper -->
-
 <!-- jQuery -->
-<script src="{{url('public/plugins/jquery/jquery.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <!-- jQuery UI 1.11.4 -->
-<script src="{{url('public/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
-<!-- Bootstrap 4 -->
-<script src="{{url('public/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
 <!-- ChartJS -->
-<script src="{{url('public/plugins/chart.js/Chart.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.10.2/Chart.min.js"></script>
+
 <!-- Sparkline -->
-<script src="{{url('public/plugins/sparklines/sparkline.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-sparklines/2.1.2/jquery.sparkline.min.js"></script>
+
 <!-- JQVMap -->
-<script src="{{url('public/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-<script src="{{url('public/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqvmap/1.5.1/jquery.vmap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqvmap/1.5.1/maps/jquery.vmap.usa.js"></script>
+
 <!-- jQuery Knob Chart -->
-<script src="{{url('public/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
-<!-- daterangepicker -->
-<script src="{{url('public/plugins/moment/moment.min.js')}}"></script>
-<script src="{{url('public/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-Knob/1.2.11/jquery.knob.min.js"></script>
+
+<!-- Moment.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+
+<!-- Daterangepicker -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.1.0/daterangepicker.js"></script>
+
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{url('public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/0.11.0/js/tempusdominus-bootstrap-4.min.js"></script>
+
 <!-- Summernote -->
-<script src="{{url('public/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.js"></script>
+
 <!-- overlayScrollbars -->
-<script src="{{url('public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/1.13.0/js/jquery.overlayScrollbars.min.js"></script>
+
 <!-- AdminLTE App -->
-<script src="{{url('public/dist/js/adminlte.js')}}"></script>
-<!-- AdminLTE for demo purposes  
-<script src="{{url('public/dist/js/demo.js')}}"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/js/adminlte.min.js"></script>
+
+<!-- AdminLTE for demo purposes -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/js/demo.js"></script> -->
+
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{url('public/dist/js/pages/dashboard.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/js/pages/dashboard.js"></script>
+
 </body>
 </html>

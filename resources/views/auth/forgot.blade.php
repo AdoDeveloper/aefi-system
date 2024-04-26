daa123d1072cc012843ebac80a68581b630992c8
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2024-04-25 17:22:04 -0700

    FORGOT UPDATED

--- resources/views/auth/forgot.blade.php
+++ resources/views/auth/forgot.blade.php
@@ -4,11 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AEFI - Recuperar contraseña</title>
-  <link rel="icon" href="{{ url('public/images/logo-uso.ico') }}" type="image/x-icon">
+  <link rel="icon" href="{{ secure_asset('images/logo-uso.ico') }}" type="image/x-icon">
+  <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <link rel="stylesheet" href="{{ url('public/plugins/fontawesome-free/css/all.min.css') }}">
-  <link rel="stylesheet" href="{{ url('public/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-  <link rel="stylesheet" href="{{ url('public/dist/css/adminlte.min.css') }}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ secure_asset('plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ secure_asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ secure_asset('dist/css/adminlte.min.css') }}">
   <style>
     body {
       background: rgb(29, 69, 131);
@@ -16,8 +20,9 @@
     }
     .login-box,
     .card {
-      border-radius: 20px; /* Ajusta el radio según tus preferencias */
-    }  
+      border-radius: 20px; /* Ajusta el radio según tus preferencias */   
+    }
+    
   </style>
 </head>
 <body class="hold-transition login-page $enable-gradients.aqua-gradient">
@@ -61,11 +66,11 @@
     </div>
   </div>
 
-  <!-- jQuery -->
-  <script src="{{ url('public/plugins/jquery/jquery.min.js') }}"></script>
-  <!-- Bootstrap 4 -->
-  <script src="{{ url('public/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <!-- AdminLTE App -->
-  <script src="{{ url('public/dist/js/adminlte.min.js') }}"></script>
+ <!-- jQuery -->
+<script src="{{ secure_asset('plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ secure_asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ secure_asset('dist/js/adminlte.min.js') }}"></script>
 </body>
 </html>
